3ec9c2819d91cb319b085ae760971f0d9f08df8d
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-01-31 13:20:30 -0700

    updated data pulled from api call

--- src/components/CoinData/CoinData.js
+++ src/components/CoinData/CoinData.js
@@ -12,7 +12,9 @@ export default class CoinData extends React.Component {
   getCoinData = async () => {
     this.setState({ isLoading: true });
     try {
-      const {data} = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+      const { data } = await axios(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      );
       this.setState({
         hasData: true,
         isLoading: false,
@@ -34,26 +36,30 @@ export default class CoinData extends React.Component {
 
   render() {
     const { hasData, hasError, isLoading, coinData } = this.state;
-    console.log(this.state.coinData)
+    console.log(coinData);
     return (
       <div>
         <div>Coin Data</div>
         {isLoading && <div>Loading data...</div>}
         {hasError && <div>error</div>}
         <div>
-          {hasData && 
-          <>
-          <div>id: {coinData.id}</div>
-          <div>symbol: {coinData.symbol}</div>
-          <div>image: {coinData.image}</div>
-          <div>current price: ${coinData.current_price}</div>
-          <div>market cap rank: {coinData.market_cap_rank}</div>
-          {/* <div>1h %: {coinData.price_change_percentage_24h}%</div> */}
-          <div>24h %: {coinData.circulating_supply}%</div>
-          {/* <div>7d %: {coinData.circulating_supply}%</div> */}
-          <div>circulating supply: {coinData.circulating_supply}%</div>
-          </>
-          }
+          {hasData && (
+            <div>
+              {coinData.map((coin) => (
+                <>
+                  <div>id: {coin.id}</div>
+                  <div>symbol: {coin.symbol}</div>
+                  <div>image: {coin.image}</div>
+                  <div>current price: ${coin.current_price}</div>
+                  <div>market cap rank: {coin.market_cap_rank}</div>
+                  {/* <div>1h %: {coin.price_change_percentage_24h}%</div> */}
+                  <div>24h %: {coin.circulating_supply}%</div>
+                  {/* <div>7d %: {coin.circulating_supply}%</div> */}
+                  <div>circulating supply: {coin.circulating_supply}%</div>
+                </>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
