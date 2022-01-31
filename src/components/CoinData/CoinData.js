34420c4432c3e0ea0bb369aca94294a99f90de3d
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-01-31 13:04:33 -0700

    added coin data

--- src/components/CoinData/CoinData.js
+++ src/components/CoinData/CoinData.js
@@ -12,12 +12,12 @@ export default class CoinData extends React.Component {
   getCoinData = async () => {
     this.setState({ isLoading: true });
     try {
-      const response = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+      const {data} = await axios("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
       this.setState({
         hasData: true,
         isLoading: false,
         hasError: false,
-        coinData: response,
+        coinData: data,
       });
     } catch (err) {
       this.setState({
@@ -34,30 +34,26 @@ export default class CoinData extends React.Component {
 
   render() {
     const { hasData, hasError, isLoading, coinData } = this.state;
-    const convert = n => {
-      if (n < 1e3) return n;
-      if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
-      if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-      if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
-      if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-    };
     console.log(this.state.coinData)
     return (
       <div>
-        <div>Global Data</div>
+        <div>Coin Data</div>
         {isLoading && <div>Loading data...</div>}
         {hasError && <div>error</div>}
         <div>
-          {/* {hasData && 
+          {hasData && 
           <>
-          <div>coins: {globalData.data.active_cryptocurrencies}</div>
-          <div>markets: {globalData.data.markets}</div>
-          <div>total market cap(usd): ${convert(globalData.data.total_market_cap.usd)}</div>
-          <div>total volume(usd): ${convert(globalData.data.total_volume.usd)}</div>
-          <div>market cap %(btc): {globalData.data.market_cap_percentage.btc.toFixed(0)}%</div>
-          <div>market cap %(eth): {globalData.data.market_cap_percentage.eth.toFixed(0)}%</div>
+          <div>id: {coinData.id}</div>
+          <div>symbol: {coinData.symbol}</div>
+          <div>image: {coinData.image}</div>
+          <div>current price: ${coinData.current_price}</div>
+          <div>market cap rank: {coinData.market_cap_rank}</div>
+          {/* <div>1h %: {coinData.price_change_percentage_24h}%</div> */}
+          <div>24h %: {coinData.circulating_supply}%</div>
+          {/* <div>7d %: {coinData.circulating_supply}%</div> */}
+          <div>circulating supply: {coinData.circulating_supply}%</div>
           </>
-          } */}
+          }
         </div>
       </div>
     );
