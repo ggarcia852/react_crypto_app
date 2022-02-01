aa04600960b20613a5d48d606979ac608f14f060
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-01-31 20:04:27 -0700

    pulled necessary coin data for landing page

--- src/components/CoinData/CoinData.js
+++ src/components/CoinData/CoinData.js
@@ -13,7 +13,7 @@ export default class CoinData extends React.Component {
     this.setState({ isLoading: true });
     try {
       const { data } = await axios(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C7d"
       );
       this.setState({
         hasData: true,
@@ -36,7 +36,6 @@ export default class CoinData extends React.Component {
 
   render() {
     const { hasData, hasError, isLoading, coinData } = this.state;
-    console.log(coinData);
     return (
       <div>
         <div>Coin Data</div>
@@ -49,14 +48,16 @@ export default class CoinData extends React.Component {
                 <>
                   <div>id: {coin.id}</div>
                   <div>symbol: {coin.symbol}</div>
+                  <div>name: {coin.name}</div>
                   <img src={coin.image} alt="coin"></img>
-                  {/* <div>image: {coin.image}</div> */}
-                  <div>current price: ${coin.current_price}</div>
+                  <div>price: ${coin.current_price}</div>
                   <div>market cap rank: {coin.market_cap_rank}</div>
-                  <div>24h %: {coin.price_change_percentage_24h.toFixed(2)}%</div>
-                  {/* <div>1h %: {coin.circulating_supply}%</div> */}
-                  {/* <div>7d %: {coin.circulating_supply}%</div> */}
+                  <div>1h: {coin.price_change_percentage_1h_in_currency.toFixed(2)}%</div>
+                  <div>24h: {coin.price_change_percentage_24h.toFixed(2)}%</div>
+                  <div>7d: {coin.price_change_percentage_7d_in_currency.toFixed(2)}%</div>
+                  <div>volume: ${coin.total_volume}</div>
                   <div>circulating supply: ${coin.circulating_supply}</div>
+                  <div>total supply: ${coin.total_supply}</div>
                 </>
               ))}
             </div>
