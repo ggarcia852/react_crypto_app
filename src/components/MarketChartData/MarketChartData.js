e91d2d9ee046076e9a41a17ea3406122a357de5b
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-02-03 12:04:34 -0700

    added charts for price and volume

--- src/components/MarketChartData/MarketChartData.js
+++ src/components/MarketChartData/MarketChartData.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
-import { ConvertTime } from "utils";
-
-export default class MarketChartData extends Component {
+import { Bar, Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+import { ConvertTime, ConvertCurrency } from "utils";
+export default class MarketChartData extends React.Component {
   state = {
     hasData: false,
     isLoading: false,
@@ -37,35 +38,69 @@ export default class MarketChartData extends Component {
 
   render() {
     const { hasData, hasError, isLoading, chartData } = this.state;
-    console.log(chartData)
     return (
       <div>
         <h1>Chart Data</h1>
         {isLoading && <div>Loading data...</div>}
         {hasError && <div>error</div>}
         <div>
-        <h2>Hourly Price Data(Bitcoin)</h2>
+          <h2>Hourly Price Data(Bitcoin)</h2>
           {hasData && (
             <div>
-            {chartData.prices.map(price=>(
-              <>
-              <div>time: {ConvertTime(price[0])}</div>
-              <div>price: {price[1].toFixed()}</div>
-              </>
-            ))}
+              <Line
+                data={{
+                  labels: chartData.prices.map((price) =>
+                    ConvertTime(price[0])
+                  ),
+                  datasets: [
+                    {
+                      label: "Bitcoin Hourly Price",
+                      data: chartData.prices.map((price) => price[1].toFixed()),
+                    },
+                  ],
+                }}
+              />
+              {/* {chartData.prices.map((price) => (
+                <>
+                  <div>time: {ConvertTime(price[0])}</div>
+                  <div>price: {price[1].toFixed()}</div>
+                </>
+              ))} */}
             </div>
           )}
         </div>
         <div>
-        <h2>Hourly Volume Data(Bitcoin)</h2>
+          <h2>Hourly Volume Data(Bitcoin)</h2>
           {hasData && (
             <div>
-            {chartData.total_volumes.map(volume=>(
-              <>
-              <div>time: {ConvertTime(volume[0])}</div>
-              <div>volume: {volume[1].toFixed()}</div>
-              </>
-            ))}
+              <Bar
+                data={{
+                  labels: chartData.total_volumes.map((volume) =>
+                    ConvertTime(volume[0])
+                  ),
+                  datasets: [
+                    {
+                      label: "Bitcoin Volume",
+                      data: chartData.total_volumes.map((volume) => volume[1]),
+                    },
+                  ],
+                }}
+                options={{
+                  scales: {
+                    y: {
+                      beginAtZero: false,
+                    },
+                  },
+                }}
+              />
+
+              {/* {chartData.total_volumes.map((volume) => (
+                <>
+                  <div>time: {ConvertTime(volume[0])}</div>
+                  <div>volume: {volume[1].toFixed()}</div>
+                  
+                </>
+              ))} */}
             </div>
           )}
         </div>
