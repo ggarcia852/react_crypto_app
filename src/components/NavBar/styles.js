a83bcfe1e83bdecb08baa103aac0c33927f16c81
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-02-05 15:14:35 -0700

    fixed suggestions

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -14,19 +14,18 @@ export const StyledLinks = styled.div`
 `
 
 export const StyledLink = styled(NavLink)`
-    display: flex;
-    text-decoration: none;
-    color: #FAFBFB;
-    font-size: 23px;
-    height: 57px;
-    width: 167px;
-    align-items: center;
-    justify-content: center;
-    border-radius: 15px;
-    &.active {
-        background: #2C2F36;
-      }
-    
+  display: flex;
+  text-decoration: none;
+  color: #FAFBFB;
+  font-size: 23px;
+  height: 57px;
+  width: 167px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  &.active {
+      background: #2C2F36;
+    }
 `
 
 export const StyledNavList = styled.ul`
@@ -38,33 +37,33 @@ export const StyledNavList = styled.ul`
 `;
 
 export const StyledNavigation = styled.div`
-      display: flex;
+  display: flex;
 
 `
 
 export const StyledInput = styled.input`
-      height: 63px;
-      width: 510px;
-      background: #2C2F36;
-      border-radius: 15px;
-      border: none;
+  height: 63px;
+  width: 510px;
+  background: #2C2F36;
+  border-radius: 15px;
+  border: none;
 `
 
 export const StyledDropdown = styled.select`
-      height: 63px;
-      width: 135px;
-      display: flex;
-      background: #2C2F36;
-      margin-left: 25px;
+  height: 63px;
+  width: 135px;
+  display: flex;
+  background: #2C2F36;
+  margin-left: 25px;
 `
 
 export const StyledButton = styled.button`
-      height: 63px;
-      width: 67px;
-      background: #2C2F36;
-      border-radius: 15px;
-      border-color: #2C2F36;
-      margin-left: 25px;
-      margin-right: 5px;
-      border: none;
+  height: 63px;
+  width: 67px;
+  background: #2C2F36;
+  border-radius: 15px;
+  border-color: #2C2F36;
+  margin-left: 25px;
+  margin-right: 5px;
+  border: none;
 `
