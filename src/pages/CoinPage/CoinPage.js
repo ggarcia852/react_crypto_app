35f3dc893fb70b4acfaad705b6e9619c88495724
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-02-13 21:02:11 -0700

    pulled all necessary data for coin page

--- src/pages/CoinPage/CoinPage.js
+++ src/pages/CoinPage/CoinPage.js
@@ -16,6 +16,7 @@ export default class CoinPage extends React.Component {
       const { data } = await axios(
         `https://api.coingecko.com/api/v3/coins/${coin}?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=true`
       );
+      console.log(data)
       this.setState({
         hasData: true,
         isLoading: false,
@@ -52,8 +53,27 @@ export default class CoinPage extends React.Component {
         {hasError && <div>{userMessage}</div>}
         {hasData && (
           <>
-            <h1>{coin.name}</h1>
-            <img src={coin.image.large} alt="coin" />
+            <h1>{coin.name}({coin.symbol.toUpperCase()})</h1>
+            <img src={coin.image.small} alt="coin" />
+            <div>{coin.links.homepage[0]}</div>
+            <div>Description: {coin.description.en}</div>
+            <div>${coin.market_data.current_price.usd}</div>
+            <div>24h% {coin.market_data.price_change_percentage_24h_in_currency.usd}%</div>
+            <div>All Time High: ${coin.market_data.ath.usd}</div>
+            <div>{coin.market_data.ath_date.usd}</div>
+            <div>All Time Low: ${coin.market_data.atl.usd}</div>
+            <div>{coin.market_data.atl_date.usd}</div>
+            <div>Market Cap: ${coin.market_data.market_cap.usd}</div>
+            <div>Fully Diluted Valuation: ${coin.market_data.fully_diluted_valuation.usd}</div>
+            <div>Volume 24h{coin.market_data.market_cap_change_24h}</div>
+            <div>Volume / Market Cap: {coin.market_data.market_cap_change_percentage_24h_in_currency.usd}%</div>
+            <div>Total Volume: {coin.market_data.total_volume.usd}</div>
+            <div>Circulating Supply: {coin.market_data.circulating_supply}</div> 
+            <div>Max Supply: {coin.market_data.max_supply}</div>
+            <div>{coin.links.blockchain_site[0]}</div>
+            <div>{coin.links.blockchain_site[1]}</div>
+            <div>{coin.links.blockchain_site[2]}</div>
+
           </>
         )}
       </>
