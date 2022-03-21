76a6d02b499652e817f5ec02d6a00efef16e92ba
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2022-03-20 18:58:37 -0700

    changed app.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CoinList, CoinPage, Portfolio } from "pages";
 import { NavBar } from "components";
 import { Container, GlobalStyle } from "GlobalStyles/styles";
 
-export default class App extends React.Component {
-  state = {
-    currency: "usd",
+const App = () => {
+  const [currency, setCurrency] = useState("usd");
+
+  const handleCurrency = (currency) => {
+    setCurrency(currency);
   };
 
-  handleCurrency = (currency) => {
-    this.setState({ currency });
-  };
+  return (
+    <Container>
+      <GlobalStyle />
+      <Router>
+        <div>
+          <NavBar handleCurrency={handleCurrency} />
+          <Switch>
+            <Route exact path="/">
+              <CoinList currency={currency} />
+            </Route>
+            {/* <Route  exact path="/coin/:coinId">
+              <CoinPage currency={currency} />
+            </Route>  */}
+            <Route
+              exact
+              path="/coin/:coinId"
+              render={(props) => <CoinPage {...props} currency={currency} />}
+            />
+            <Route path="/portfolio" component={Portfolio} />
+          </Switch>
+        </div>
+      </Router>
+    </Container>
+  );
+};
 
-  render() {
-    return (
-      <Container>
-        <GlobalStyle />
-        <Router>
-          <div>
-            <NavBar handleCurrency={this.handleCurrency} />
-            <Switch>
-              <Route exact path="/">
-                <CoinList currency={this.state.currency} />
-              </Route>
-              <Route
-                exact
-                path="/coin/:coinId"
-                render={(props) => (
-                  <CoinPage {...props} currency={this.state.currency} />
-                )}
-              />
-              <Route path="/portfolio" component={Portfolio} />
-            </Switch>
-          </div>
-        </Router>
-      </Container>
-    );
-  }
-}
+export default App;
+// export default class App extends React.Component {
+//   state = {
+//     currency: "usd",
+//   };
+
+//   handleCurrency = (currency) => {
+//     this.setState({ currency });
+//   };
+
+//   render() {
+//     return (
+//       <Container>
+//         <GlobalStyle />
+//         <Router>
+//           <div>
+//             <NavBar handleCurrency={this.handleCurrency} />
+//             <Switch>
+//               <Route exact path="/">
+//                 <CoinList currency={this.state.currency} />
+//               </Route>
+//               <Route
+//                 exact
+//                 path="/coin/:coinId"
+//                 render={(props) => (
+//                   <CoinPage {...props} currency={this.state.currency} />
+//                 )}
+//               />
+//               <Route path="/portfolio" component={Portfolio} />
+//             </Switch>
+//           </div>
+//         </Router>
+//       </Container>
+//     );
+//   }
+// }
